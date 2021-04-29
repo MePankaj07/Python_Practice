cc37d825ca270f38d3ea3a4f6a3e4581c0924c48
Pankaj Bhardwaj <pankajbhardwaj994@gmail.com>
2021-04-29 11:26:05 +0530

    kilometer to miles

---
+++ KmToMile.py
@@ -0,0 +1,6 @@
+def Conversion():
+    KiloMet = float(input("Ente a number to convert it to miles : "))
+    Conv_Miles = 0.621371
+    FinalConvert = KiloMet * Conv_Miles
+    print(f"{KiloMet} Kilometer to {FinalConvert} Miles")
+Conversion()
