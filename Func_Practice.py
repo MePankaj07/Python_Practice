e619d16a7831188eba133dcd6da332561c9f5d1b
Pankaj Bhardwaj <pankajbhardwaj994@gmail.com>
2021-04-29 11:29:42 +0530

    Functions practice in python

---
+++ Func_Practice.py
@@ -0,0 +1,20 @@
+# Prob 1 :  Write a program to print sum of n natural numbers ? 
+# def Sum(n):
+#     if n<=1:
+#         return n
+#     else:
+#         return n + Sum(n-1)
+
+# print(Sum(10))
+
+# Prob 2 : Write a program to print following patternn.
+
+# ***
+# **
+# *
+
+# def PatternStar(n):
+#     for i in range(0,n):
+#         print("*"*(n-i))
+# print(PatternStar(3))
+
