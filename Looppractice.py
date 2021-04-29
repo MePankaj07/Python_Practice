211baf6c8cd220e1af9220fd38d57512cdadc6a6
Pankaj Bhardwaj <pankajbhardwaj994@gmail.com>
2021-04-29 11:28:27 +0530

    Loops practice questions in python with solution

---
+++ Looppractice.py
@@ -0,0 +1,53 @@
+# Problem 1 : Write a program to write a multiplication table entered by the user?
+
+# num = int(input("Enter a Number to print a table : "))
+# for i in range(0,11):
+#     print(str(num) + "X" + str(i) + "=" + str(num*i))
+
+# Problem 2: write a program to greet all the persons names stored in  a list l1 and which starts with s?
+#       l1=["harry" "soham" "sachin" "rahul"]
+
+# l1=["harry", "soham", "sachin" ,"rahul"]
+# for name in l1:
+#     if name.startswith("s"):
+#         print("Hello " + name)
+
+# Problem 3: Attempt problem 1 using while loop?
+
+# num = int(input("Enter a number to find its table : "))
+# i=0
+# while(i<=10):
+#     print(f"{num} X {i} = {num*i}")
+#     i=i+1
+
+
+
+
+# Problem 4 : write a program to find the sum of first n natural numbers entered by the user?
+
+# num = int(input("Enter a number : "))
+# sum=0
+# while(num>0):
+#     sum += num
+#     num = num-1
+# print(sum)
+
+# Problem 5 : Write a program to find a factorial of a given number?
+
+# num= int(input("Enter a number : "))
+# factorial = 1
+# for i in range(1,num+1):
+#     factorial = factorial*i
+# print(f"The factorial of {num} is {factorial}")
+
+# Problem 6 : Write a program to print the following star pattern?
+
+    #    *
+    #    **
+    #    ***   (n=3)
+
+# n= int(input("Enter a number: "))
+# for i in range(1,n+1):
+
+#     print(i*"*")
+
