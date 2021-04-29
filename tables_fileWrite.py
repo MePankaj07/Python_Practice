4ce9bd9432a9d5b4e301ceddac2143cc1f11a3d6
Pankaj Bhardwaj <pankajbhardwaj994@gmail.com>
2021-04-29 11:15:00 +0530

    Delete tables_fileWrite.py

--- tables_fileWrite.py
+++
@@ -1,7 +0,0 @@
-# Tables 
-for i in range(2,20):
-    with open(f"Tables/tables_fileWrite{i}.txt", "w") as f:
-        for j in range(1,11):
-            f.write(f"{i}X{j}={i*j}")
-            if j!=10:
-                f.write("\n")
