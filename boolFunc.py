c7684cfc11238abc870533a39908f335cfb18aa8
Pankaj Bhardwaj <pankajbhardwaj994@gmail.com>
2021-05-29 21:48:58 +0530

    BoolReversal

---
+++ boolFunc.py
@@ -0,0 +1,12 @@
+
+def reverseBool():
+    inpBool = input("Bool : ")
+    if inpBool == "True":
+        print("False")
+    elif inpBool == "False":
+        print("True")
+    else:
+        print("boolean expected")
+
+
+reverseBool()
